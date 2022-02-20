24d7d971da4826636064ba476a76a75f63c642c5
Alice2410 <devo4ka2410@gmail.com>
2022-02-20 19:13:36 +0300

    Исправление ошибок

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 //источник: https://vk.com/away.php?to=https%3A%2F%2Fschoolsw3.com%2Fjs%2Fjs_random.php&el=snippet
 
 function getRndInteger(min, max) {
-  if ((min<0) || (max<0)) return("Введите неотрицательные значения");
+  if ((min<0) || (max<0)) {
+    return('Введите неотрицательные значения');
+  }
 
   if (min >= max) {
-    console.log ("Введите корректные максимальное и минимальное значения");
+    return ('Введите корректные максимальное и минимальное значения');
   } else {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
@@ -15,5 +17,5 @@ function checkCommentLength (comment, maxCommentLength) {
 }
 
 getRndInteger(1,2);
-checkCommentLength("aaa",4);
+checkCommentLength('aaa',4);
 
